f1907a3385a321b633d79783ce3527c8ff184b24
codesuraj12 <160992864+codesuraj12@users.noreply.github.com>
2025-03-03 14:34:35 +0530

    Fixed lock issue

--- src/Component/Front.jsx
+++ src/Component/Front.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 const Front = () => {
+const tech = 'Technology'.split('')
+
   return (
     <div>
            <motion.h1
@@ -10,21 +12,28 @@ const Front = () => {
         animate={{ opacity: 1, y: 0 }} 
         transition={{ duration: 1, ease: "easeOut" }} 
       >
-        Next-Gen <motion.span
-          className="inline-block text-white" 
-          animate={{ y: [0, -5, 0, -5, 0] }} 
-          transition={{
-            repeat: Infinity, 
-            repeatType: "reverse", 
-            duration: 1.9,
-            ease: "easeOut",
-          }}
-        >
-          Technology
-        </motion.span>{" "} Solutions
+       <span> Next-Gen </span>
+         {tech.map((leter, index)=>(
+                    <motion.span className='text-white'
+                     key={index} 
+                     initial={{ opacity: 0, y: 10 }}
+                     animate={{ opacity: 1, y: [0, -10, 0] }} 
+                     transition={{ 
+                       delay: index * 0.4, 
+                       duration: 0.5, 
+                      
+                       repeatType: "reverse", 
+                       ease: "easeInOut" 
+                     }}
+                    >
+                    { leter }
+                    </motion.span>
+                  ))}
+         
+    <span> Solutions</span>     
       </motion.h1>
       <motion.p
-        className="hero-text text-white text-[4vw] md:text-[1.2rem] lg:text-[1.5rem] mx-auto whitespace-normal max-w-[50vw]"
+        className="hero-text text-white text-[4vw] md:text-[1.2rem] lg:text-[1.5rem] mx-auto whitespace-normal sm:max-w-[50vw]"
         initial={{ opacity: 0, y: 30 }} 
         animate={{ opacity: 1, y: 0 }} 
         transition={{ duration: 1, ease: "easeOut", delay: 0.3 }} 
