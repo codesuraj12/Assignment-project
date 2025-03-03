5f9b3020f24258205e72eeab3de326108f9b3c48
codesuraj12 <160992864+codesuraj12@users.noreply.github.com>
2025-03-03 09:55:02 +0530

    Updated code with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
 
 
-<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center mt-10">
+<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center ">
  <Front/>
   <div className='flex gap-3 justify-center'>
 
